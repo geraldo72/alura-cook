4bacd2f1947c817b3e8db7a14191631f229724b8
geraldo72 <13860067+geraldo72@users.noreply.github.com>
2023-01-31 00:03:17 -0300

    SPA course

---
+++ src/routes/+page.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    import "../app.css";
+    import Cabecalho from "$components/Cabecalho.svelte";
+    import MinhaLista from "$components/MinhaLista.svelte";
+    import Titulo from "$components/Titulo.svelte";
+
+    import categorias from "$lib/json/categorias.json";
+    import Categoria from "$components/Categoria.svelte";
+    import Tag from "$components/Tag.svelte";
+    import Rodape from "$components/Rodape.svelte";
+
+    let ingredientes: string[] = [];
+
+    function adicionarIngrediente(evento: CustomEvent<string>) {
+        const ingrediente = evento.detail;
+        ingredientes = [...ingredientes, ingrediente];
+    }
+    function removerIngrediente(evento: CustomEvent<string>) {
+        const ingrediente = evento.detail;
+        ingredientes = ingredientes.filter((item) => item !== ingrediente);
+    }
+</script>
+
+<svelte:head>
+    <title>Alura Cook</title>
+</svelte:head>
+
+<div class="container-principal">
+    <Cabecalho />
+    <div class="estilo-principal">
+        {#if ingredientes.length}
+            <div class="minha-lista-container">
+                <MinhaLista {ingredientes} />
+
+                <div class="divisoria" />
+            </div>
+        {/if}
+        <main>
+            <Titulo tag="h1">Ingredientes</Titulo>
+            <div class="info">
+                <p>
+                    Selecione abaixo os ingredientes que você deseja usar nesta
+                    refeição:
+                </p>
+                <p>
+                    *Atenção: consideramos que você tenha em casa sal, pimenta e
+                    água.
+                </p>
+            </div>
+            <ul class="categorias">
+                {#each categorias as categoria (categoria.nome)}
+                    <Categoria
+                        {categoria}
+                        on:adicionarIngrediente={adicionarIngrediente}
+                        on:removerIngrediente={removerIngrediente}
+                    />
+                {/each}
+            </ul>
+
+            <div class="buscar-receitas">
+                <a href="/receitas">
+                    <Tag ativa tamanho="lg">Buscar Receitas</Tag>
+                </a>
+            </div>
+        </main>
+    </div>
+
+    <Rodape />
+</div>
+
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/receitas">Receitas</a>
+
+<img src="/imagens/receitas/panqueca.png" alt="panqueca"/> -->
+<style>
+    .container-principal {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    .estilo-principal {
+        text-align: center;
+        padding: 0 5vw 3.375rem;
+        flex: 1;
+    }
+
+    .minha-lista-container {
+        margin-bottom: 2rem;
+    }
+    .divisoria {
+        width: 40vw;
+        height: 2px;
+        background-color: var(--verde);
+        margin: 0 auto;
+    }
+
+    .info {
+        margin-bottom: 3.375rem;
+    }
+
+    .info > p {
+        line-height: 2rem;
+    }
+    .categorias {
+        margin-bottom: 4.6875rem;
+
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 1.5rem;
+    }
+    .buscar-receitas {
+        display: flex;
+        justify-content: center;
+    }
+</style>
